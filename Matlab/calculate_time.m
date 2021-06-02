3affe18c683f2c9d847ac0456f2fb170ba0e8688
xjchen <xjchen@gzhu.edu.cn>
2021-06-02 10:34:57 +0800

    update code 202106021034

--- Matlab/calculate_time.m
+++ Matlab/calculate_time.m
@@ -7,6 +7,7 @@ cd "Z:/Matlab" % 引号内输入Matlab文件所在位置
 	
 feature('DefaultCharacterSet', 'UTF-8');
 
+% 此代码计算交通网络的城际运输时间,以两年的高速公路和铁路两种交通网络数据为例，如需增加高铁路、航空运输、水路则需要修改代码。
 
 %% 导入图片格式的交通网络数据，并转为01矩阵，1表示线路，0表示非线路
 k = 2; % 有多少年的交通网络地图就输入多少，例如这里要保存2年的数据
@@ -39,25 +40,31 @@ coor_map = readtable('../Network/four_corner_coordinates.xls','ReadVariableNames
 
 
 % 测试北京经纬度是否正确
-imagesc(railway_map{2})
+imagesc(railway_map{2}) 
 hold on 
 plot(city_xy.railway_col(1), city_xy.railway_row(1),'.r', "MarkerSize", 20);
 
 %% 设置交通方式的单位速度
 speed_railway = 160; % 铁路160公里/小时
 speed_highway = 90; % 高速公路90公里/小时
-speed_offroad = 10; 
+
+% 公里/小时转为像素/小时
+xmin = coor_map{1,1};  xmax = coor_map{1,3};  width = coor_map{1,6};
+km_per_pixel = (xmax - xmin)*111/width;  % 一经纬度大约等于111公里，每个像素大约4公里
+time_per_pixel_railway = km_per_pixel/speed_railway; % 火车经过一个像素需要多长时间
+time_per_pixel_highway = km_per_pixel/speed_highway;
 
 % 设置相对速度，高速公路速度标准化为1
-relate_speed_highway = speed_highway/speed_highway;
-relate_speed_railway = speed_railway/speed_highway;
-relate_speed_offroad = speed_offroad/speed_highway;
+relate_time_highway = time_per_pixel_highway/time_per_pixel_highway;
+relate_time_railway = time_per_pixel_railway/time_per_pixel_highway;
+relate_time_offroad = 2; % 设置一个比铁路和高速公路时间更大的数值, graydist寻找时间
 
 %% 计算交通时间
 
+
 % 高速公路
 % 根据交通速度设置交通网络权重
-weight_highway = weight_map(coor_map,highway_map,relate_speed_highway,relate_speed_offroad);
+weight_highway = weight_map(highway_map,relate_time_highway,relate_time_offroad);
 
 n = size(weight_highway,1); % year
 m = size(city_xy,1); % city
@@ -67,7 +74,7 @@ Col = city_xy.highway_col;
 Row = city_xy.highway_row;
 
 % 计算交通时间
-for ix=1:prod(iterations)
+for ix=1:prod(iterations) % 如需加快速度可用parfor做并行计算
     [nn,mm]=ind2sub(iterations,ix);
     time = graydist_tool(weight_highway,Col,Row,nn,mm);
     time_highway(ix) = {time}; 
@@ -76,7 +83,7 @@ end
 
 % 铁路 
 % 根据交通速度设置交通网络权重
-weight_railway = weight_map(coor_map,railway_map,relate_speed_railway,relate_speed_offroad);
+weight_railway = weight_map(railway_map,relate_time_railway,relate_time_offroad);
 
 n = size(weight_railway,1); % year
 m = size(city_xy,1); % city
@@ -87,7 +94,7 @@ Row = city_xy.railway_row;
 
 % 计算交通时间
 for ix=1:prod(iterations)
-    [nn,mm]=ind2sub(iterations,ix);
+    [nn,mm]=ind2sub(iterations,ix); 
     time = graydist_tool(weight_railway,Col,Row,nn,mm);
     time_railway(ix) = {time}; 
 end
